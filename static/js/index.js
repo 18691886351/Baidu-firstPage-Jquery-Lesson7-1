aeb05bd55bbd52a3a8f3956d903aa6171849f07c
qianl <450374167@qq.com>
2016-08-21 17:08:29 +0800

    20160821第二次提交

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,40 @@
-/*
-实现了导航栏目中“设置”的鼠标moveover以及moveout事件：鼠标moveover的时候显示，moveout消失
-*/
-function settingMenuHideOrdisplay() {
-    if(document.getElementById("setting_menu").className == "setting_menu_hide"){
-    	document.getElementById("setting_menu").className="setting_menu_show"
-    }else{
-    	document.getElementById("setting_menu").className="setting_menu_hide"
-    }
+var nav_setting = document.getElementById("setting");
+var nav_setting_menu = document.getElementById("setting_menu");
+
+var products = document.getElementById("products");
+var products_menu = document.getElementById("products_menu");
+
+
+nav_setting.onmouseover = function() {
+
+    nav_setting_menu.style.display = "block"
+
+}
+nav_setting.onmouseout = function() {
+
+    nav_setting_menu.style.display = "none"
 }
 
-/*
-实现了导航栏目中“更多”的鼠标moveover以及moveout事件：鼠标moveover的时候显示，moveout消失
-*/
-function productsMenuHideOrdisplay() {
-    if(document.getElementById("products_menu").className == "products_menu_hide"){
-    	document.getElementById("products_menu").className="products_menu_show"
-    }else{
-    	document.getElementById("products_menu").className="products_menu_hide"
-    }
+products.onmouseover = function() {
+
+    products_menu.style.display = "block"
+
 }
+products.onmouseout = function() {
+
+    products_menu.style.display = "none"
+}
+
 
 
 /*
 实现了搜索栏获得焦点后改变其父级元素的边框颜色,失去焦点还原
 */
 function changeInputParentBorderColor() {
-    if(document.getElementById("input_div").className == "text_span"){
-        document.getElementById("input_div").className="text_span_focus"
-    }else{
-        document.getElementById("input_div").className="text_span"
+    if (document.getElementById("input_div").className == "text_span") {
+        document.getElementById("input_div").className = "text_span_focus"
+    } else {
+        document.getElementById("input_div").className = "text_span"
     }
 }
 
@@ -37,10 +43,10 @@ function changeInputParentBorderColor() {
 实现了导航栏目中“百度一下”的鼠标moveover以及moveout事件
 */
 function baiduBntOnmouseoverOrOut() {
-    if(document.getElementById("baidu_bnt").className == "baidu_bnt_onmouseout"){
-        document.getElementById("baidu_bnt").className="baidu_bnt_onmouseover"
-    }else{
-        document.getElementById("baidu_bnt").className="baidu_bnt_onmouseout"
+    if (document.getElementById("baidu_bnt").className == "baidu_bnt_onmouseout") {
+        document.getElementById("baidu_bnt").className = "baidu_bnt_onmouseover"
+    } else {
+        document.getElementById("baidu_bnt").className = "baidu_bnt_onmouseout"
     }
 }
 
@@ -49,9 +55,9 @@ function baiduBntOnmouseoverOrOut() {
 实现了导航栏目中“搜索栏camera”的鼠标moveover以及moveout事件
 */
 function cameramouseoverOrOut() {
-    if(document.getElementById("camera").className == "camera_onmouseout"){
-        document.getElementById("camera").className="camera_onmouseover"
-    }else{
-        document.getElementById("camera").className="camera_onmouseout"
+    if (document.getElementById("camera").className == "camera_onmouseout") {
+        document.getElementById("camera").className = "camera_onmouseover"
+    } else {
+        document.getElementById("camera").className = "camera_onmouseout"
     }
 }
